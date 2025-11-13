255fb7dcd0655a19c9cff04e262d907dd383b075
xusanikromov73-glitch <xusanikromov73@gmail.com>
2025-11-13 19:38:40 +0500

    asvdsdvasdvasdv

--- src/components/BoxHusan/BoxHusan.jsx
+++ src/components/BoxHusan/BoxHusan.jsx
@@ -1,37 +1,37 @@
 import React from 'react'
 import "./BoxHusan.css"
-
+import image from "/public/img2.png"
 function BoxHusan() {
   return (
     <div>
       <div className="boxes">
         <div className="box">
-          <img src="/img.png" />
+          <img src={image} />
           <h3>Unraveling the Mysteries of the Ocean</h3>
           <p>From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration. </p>
           <button>Read now</button>
         </div>
         <div className="box">
-          <img src="/img.png" />
+          <img src={image} />
           <h3>Unraveling the Mysteries of the Ocean</h3>
           <p>From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration. </p>
           <button>Read now</button>
         </div>
         <div className="box">
-          <img src="/img.png" />
+          <img src={image} />
           <h3>Unraveling the Mysteries of the Ocean</h3>
           <p>From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration. </p>
           <button>Read now</button>
         </div>
         <div className="box">
-          <img src="/img.png" />
+          <img src={image} />
           <h3>Unraveling the Mysteries of the Ocean</h3>
           <p>From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration. </p>
           <button>Read now</button>
         </div>
         <div className="box">
 
-          <img src="/img.png" />
+          <img src={image} />
           <h3>Unraveling the Mysteries of the Ocean</h3>
           <p>From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration. </p>
           <button>Read now</button>
