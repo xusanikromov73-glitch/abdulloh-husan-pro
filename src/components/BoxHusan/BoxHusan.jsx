0322ecb2ae7a6cc9fbc2cdba9c86bf257e0f2cd8
xusanikromov73-glitch <xusanikromov73@gmail.com>
2025-11-13 19:35:01 +0500

    asdvasdvasvasdv

--- src/components/BoxHusan/BoxHusan.jsx
+++ src/components/BoxHusan/BoxHusan.jsx
@@ -1,18 +1,42 @@
 import React from 'react'
 import "./BoxHusan.css"
-import image from "/public/img.png"
+
 function BoxHusan() {
   return (
     <div>
-        <div className="boxes">
-            <div className="box">
-              <img src={image} alt="" />
-            </div>
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
+      <div className="boxes">
+        <div className="box">
+          <img src="/img.png" />
+          <h3>Unraveling the Mysteries of the Ocean</h3>
+          <p>From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration. </p>
+          <button>Read now</button>
         </div>
+        <div className="box">
+          <img src="/img.png" />
+          <h3>Unraveling the Mysteries of the Ocean</h3>
+          <p>From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration. </p>
+          <button>Read now</button>
+        </div>
+        <div className="box">
+          <img src="/img.png" />
+          <h3>Unraveling the Mysteries of the Ocean</h3>
+          <p>From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration. </p>
+          <button>Read now</button>
+        </div>
+        <div className="box">
+          <img src="/img.png" />
+          <h3>Unraveling the Mysteries of the Ocean</h3>
+          <p>From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration. </p>
+          <button>Read now</button>
+        </div>
+        <div className="box">
+
+          <img src="/img.png" />
+          <h3>Unraveling the Mysteries of the Ocean</h3>
+          <p>From the mesmerizing marine life that inhabits the coral reefs to the vast and uncharted depths that hold untold secrets, we embark on a journey of discovery and exploration. </p>
+          <button>Read now</button>
+        </div>
+      </div>
     </div>
   )
 }
